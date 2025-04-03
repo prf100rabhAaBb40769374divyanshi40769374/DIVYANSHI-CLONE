d7f301934541e88fdd78e95f63692fd27e4172ed
BADNAM XD <yadavxd746@gmail.com>
2025-04-03 14:14:55 +0530

    Update ping.py

--- PROMUSIC/plugins/tools/ping.py
+++ PROMUSIC/plugins/tools/ping.py
@@ -16,7 +16,7 @@ from config import BANNED_USERS
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
-        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        video="https://files.catbox.moe/gwilak.mp4",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await PRO.ping()
