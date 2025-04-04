3e67afa7c83aadff0ca7e1b515963313f93b7bfc
BADNAM XD <yadavxd746@gmail.com>
2025-04-04 11:43:18 +0530

    Update userbot.py

--- PROMUSIC/core/userbot.py
+++ PROMUSIC/core/userbot.py
@@ -23,8 +23,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("PURVI_SUPPORT")
-                await self.one.join_chat("PURVI_UPDATES")
+                await self.one.join_chat("@TEAM_BADNAM_BOTS")
+                await self.one.join_chat("@KRITI_UPDATE")
             except:
                 pass
             assistants.append(1)
