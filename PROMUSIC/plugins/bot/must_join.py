675cc5a365711c848be39542cb7efdf64d612155
BADNAM XD <yadavxd746@gmail.com>
2025-04-04 11:47:36 +0530

    Update must_join.py

--- PROMUSIC/plugins/bot/must_join.py
+++ PROMUSIC/plugins/bot/must_join.py
@@ -26,7 +26,7 @@ async def must_join_channel(app: Client, msg: Message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("• ᴊᴏɪɴ •", url=link),
+                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/KRITI_UPDATE"),
                                 InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/TEAM_BADNAM_BOTS"),
                             ]
                         ]
