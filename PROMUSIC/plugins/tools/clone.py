bbdb9d2c5c727f56c4f159c76f34d7446767c135
Why You Visit My Profile 😂 <153857610+prf100rabhAaBb40769374divyanshi40769374@users.noreply.github.com>
2025-04-06 02:28:38 +0530

    [ 𝗣𝗥𝚯𝗙𝗘𝗦𝗦𝚯𝗥 ][ ѕαиαтαиι ]

--- PROMUSIC/plugins/tools/clone.py
+++ PROMUSIC/plugins/tools/clone.py
@@ -102,8 +102,8 @@ async def clone_txt(client, message, _):
                 "name": bot.first_name,
                 "token": bot_token,
                 "username": bot.username,
-                "channel": "THE_INCRICIBLE",
-                "support": "ZOYU_SUPPORT",
+                "channel": "ProfessorSourabh",
+                "support": "ProfessorXSupport",
                 "premium" : False,
                 "Date" : False,
             }
